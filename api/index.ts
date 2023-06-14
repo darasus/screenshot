d4dbbac565a7c20b2e8465adea305902ffdfb7a6
Ilya Daraseliya <idarase@gmail.com>
2023-06-14 07:49:00 +0200

    fixes

--- api/index.ts
+++ api/index.ts
@@ -1,23 +1,39 @@
-import { getScreenshot } from "./_lib/puppeteer";
+import {getScreenshot} from './_lib/puppeteer';
 
 module.exports = async (req, res) => {
-  if (!req.query.url) return res.status(400).send("No url query specified.");
-  if (!checkUrl(req.query.url)) return res.status(400).send("Invalid url query specified.");
+  if (!req.query.url) {
+    return res.status(400).send('No url query specified.');
+  }
+
+  if (!checkUrl(req.query.url)) {
+    return res.status(400).send('Invalid url query specified.');
+  }
+
   try {
-    const file = await getScreenshot(req.query.url, req.query.width, req.query.height);
-    res.setHeader("Content-Type", "image/png");
-    res.setHeader("Cache-Control", "public, immutable, no-transform, s-maxage=86400, max-age=86400");
+    const file = await getScreenshot(
+      req.query.url,
+      req.query.width,
+      req.query.height
+    );
+    res.setHeader('Content-Type', 'image/png');
+    res.setHeader(
+      'Cache-Control',
+      `s-maxage=10, stale-while-revalidate=${10 * 60 * 1000}`
+    );
     res.status(200).end(file);
   } catch (error) {
-    console.error(error)
-    res.status(500).send("The server encountered an error. You may have inputted an invalid query.");
+    console.error(error);
+    res
+      .status(500)
+      .send(
+        'The server encountered an error. You may have inputted an invalid query.'
+      );
   }
-}
+};
 
-function checkUrl(string, hostname) {
-  var url = "";
+function checkUrl(string) {
   try {
-    url = new URL(string);
+    new URL(string).toString();
   } catch (error) {
     return false;
   }
